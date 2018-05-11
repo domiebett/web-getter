3d5a83d2778222eeabe23b5cf97e5150c59444b6
DomieBett <dbett49@gmail.com>
2018-05-11 15:53:13 +0300

    Second commit

--- getter/getter.py
+++ getter/getter.py
@@ -4,10 +4,13 @@ from flask import Blueprint, Markup, render_template
 
 getterBlueprint = Blueprint('getter', __name__, template_folder='templates')
 http = urllib3.PoolManager()
+mainWeb = None
 
 @getterBlueprint.route('/<website>', methods=['GET'])
 def webGetter(website):
     soup = getWebsite(website)
+    for a in soup.findAll('a'):
+        a['href'] = '{}{}'.format(website, a['href'])
     web = Markup(soup)
     return render_template('getter.html', website=web)
 
